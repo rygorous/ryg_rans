0e2effadf752217125cde6d9e6628b344bed4d84
Fabian Giesen <rygorous@gmail.com>
2014-02-09 01:07:09 -0800

    Word-aligned SIMD example

--- main_simd.cpp
+++ main_simd.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <assert.h>
 
-#include "rans_byte.h"
+#include "rans_word_sse41.h"
 
 #include <intrin.h>
 
@@ -146,30 +146,21 @@ int main()
     size_t in_size;
     uint8_t* in_bytes = read_file("book1", &in_size);
 
-    static const uint32_t prob_bits = 14;
-    static const uint32_t prob_scale = 1 << prob_bits;
-
     SymbolStats stats;
     stats.count_freqs(in_bytes, in_size);
-    stats.normalize_freqs(prob_scale);
+    stats.normalize_freqs(RANS_WORD_M);
 
-    // cumlative->symbol table
-    // this is super brute force
-    uint8_t cum2sym[prob_scale];
+    // init decoding tables
+    RansWordTables tab;
     for (int s=0; s < 256; s++)
-        for (uint32_t i=stats.cum_freqs[s]; i < stats.cum_freqs[s+1]; i++)
-            cum2sym[i] = s;
+        RansWordTablesInitSymbol(&tab, (uint8_t)s, stats.cum_freqs[s], stats.freqs[s]);
 
-    static size_t out_max_size = 32<<20; // 32MB
+    size_t out_max_size = in_size + (in_size >> 3) + 128;
     uint8_t* out_buf = new uint8_t[out_max_size];
     uint8_t* dec_bytes = new uint8_t[in_size];
 
     // try rANS encode
-    uint8_t *rans_begin;
-    RansSymbol syms[256];
-
-    for (int i=0; i < 256; i++)
-        RansSymbolInit(&syms[i], stats.cum_freqs[i], stats.freqs[i]);
+    uint16_t *rans_begin;
 
     // ---- regular rANS encode/decode. Typical usage.
 
@@ -180,36 +171,35 @@ int main()
         double start_time = timer();
         uint64_t enc_start_time = __rdtsc();
 
-        RansState rans;
-        RansEncInit(&rans);
+        RansWordEnc rans = RansWordEncInit();
 
-        uint8_t* ptr = out_buf + out_max_size; // *end* of output buffer
+        uint16_t* ptr = (uint16_t *) (out_buf + out_max_size); // *end* of output buffer
         for (size_t i=in_size; i > 0; i--) { // NB: working in reverse!
             int s = in_bytes[i-1];
-            RansEncPutSymbol(&rans, &ptr, &syms[s], prob_bits);
+            RansWordEncPut(&rans, &ptr, stats.cum_freqs[s], stats.freqs[s]);
         }
-        RansEncFlush(&rans, &ptr);
+        RansWordEncFlush(&rans, &ptr);
         rans_begin = ptr;
 
         uint64_t enc_clocks = __rdtsc() - enc_start_time;
         double enc_time = timer() - start_time;
         printf("%lld clocks, %.1f clocks/symbol (%5.1fMiB/s)\n", enc_clocks, 1.0 * enc_clocks / in_size, 1.0 * in_size / (enc_time * 1048576.0));
     }
-    printf("rANS: %d bytes\n", (int) (out_buf + out_max_size - rans_begin));
+    printf("rANS: %d bytes\n", (int) (out_buf + out_max_size - (uint8_t *)rans_begin));
 
     // try rANS decode
     for (int run=0; run < 5; run++) {
         double start_time = timer();
         uint64_t dec_start_time = __rdtsc();
 
-        RansState rans;
-        uint8_t* ptr = rans_begin;
-        RansDecInit(&rans, &ptr);
+        RansWordDec rans;
+        uint16_t* ptr = rans_begin;
+        RansWordDecInit(&rans, &ptr);
 
         for (size_t i=0; i < in_size; i++) {
-            uint32_t s = cum2sym[RansDecGet(&rans, prob_bits)];
+            uint8_t s = RansWordDecSym(&rans, &tab);
             dec_bytes[i] = (uint8_t) s;
-            RansDecAdvanceSymbol(&rans, &ptr, &syms[s], prob_bits);
+            RansWordDecRenorm(&rans, &ptr);
         }
 
         uint64_t dec_clocks = __rdtsc() - dec_start_time;
@@ -233,60 +223,133 @@ int main()
         double start_time = timer();
         uint64_t enc_start_time = __rdtsc();
 
-        RansState rans0, rans1;
-        RansEncInit(&rans0);
-        RansEncInit(&rans1);
+        RansWordEnc rans0 = RansWordEncInit();
+        RansWordEnc rans1 = RansWordEncInit();
 
-        uint8_t* ptr = out_buf + out_max_size; // *end* of output buffer
+        uint16_t* ptr = (uint16_t *)(out_buf + out_max_size); // *end* of output buffer
 
         // odd number of bytes?
         if (in_size & 1) {
             int s = in_bytes[in_size - 1];
-            RansEncPutSymbol(&rans0, &ptr, &syms[s], prob_bits);
+            RansWordEncPut(&rans0, &ptr, stats.cum_freqs[s], stats.freqs[s]);
         }
 
         for (size_t i=(in_size & ~1); i > 0; i -= 2) { // NB: working in reverse!
             int s1 = in_bytes[i-1];
             int s0 = in_bytes[i-2];
-            RansEncPutSymbol(&rans1, &ptr, &syms[s1], prob_bits);
-            RansEncPutSymbol(&rans0, &ptr, &syms[s0], prob_bits);
+            RansWordEncPut(&rans1, &ptr, stats.cum_freqs[s1], stats.freqs[s1]);
+            RansWordEncPut(&rans0, &ptr, stats.cum_freqs[s0], stats.freqs[s0]);
         }
-        RansEncFlush(&rans1, &ptr);
-        RansEncFlush(&rans0, &ptr);
+        RansWordEncFlush(&rans1, &ptr);
+        RansWordEncFlush(&rans0, &ptr);
         rans_begin = ptr;
 
         uint64_t enc_clocks = __rdtsc() - enc_start_time;
         double enc_time = timer() - start_time;
         printf("%lld clocks, %.1f clocks/symbol (%5.1fMiB/s)\n", enc_clocks, 1.0 * enc_clocks / in_size, 1.0 * in_size / (enc_time * 1048576.0));
     }
-    printf("interleaved rANS: %d bytes\n", (int) (out_buf + out_max_size - rans_begin));
+    printf("interleaved rANS: %d bytes\n", (int) (out_buf + out_max_size - (uint8_t*)rans_begin));
 
     // try interleaved rANS decode
     for (int run=0; run < 5; run++) {
         double start_time = timer();
         uint64_t dec_start_time = __rdtsc();
 
-        RansState rans0, rans1;
-        uint8_t* ptr = rans_begin;
-        RansDecInit(&rans0, &ptr);
-        RansDecInit(&rans1, &ptr);
+        RansWordDec rans0, rans1;
+        uint16_t* ptr = rans_begin;
+        RansWordDecInit(&rans0, &ptr);
+        RansWordDecInit(&rans1, &ptr);
 
         for (size_t i=0; i < (in_size & ~1); i += 2) {
-            uint32_t s0 = cum2sym[RansDecGet(&rans0, prob_bits)];
-            uint32_t s1 = cum2sym[RansDecGet(&rans1, prob_bits)];
+            uint8_t s0 = RansWordDecSym(&rans0, &tab);
+            uint8_t s1 = RansWordDecSym(&rans1, &tab);
             dec_bytes[i+0] = (uint8_t) s0;
             dec_bytes[i+1] = (uint8_t) s1;
-            RansDecAdvanceSymbolStep(&rans0, &syms[s0], prob_bits);
-            RansDecAdvanceSymbolStep(&rans1, &syms[s1], prob_bits);
-            RansDecRenorm(&rans0, &ptr);
-            RansDecRenorm(&rans1, &ptr);
+            RansWordDecRenorm(&rans0, &ptr);
+            RansWordDecRenorm(&rans1, &ptr);
         }
 
         // last byte, if number of bytes was odd
         if (in_size & 1) {
-            uint32_t s0 = cum2sym[RansDecGet(&rans0, prob_bits)];
+            uint8_t s0 = RansWordDecSym(&rans0, &tab);
             dec_bytes[in_size - 1] = (uint8_t) s0;
-            RansDecAdvanceSymbol(&rans0, &ptr, &syms[s0], prob_bits);
+        }
+
+        uint64_t dec_clocks = __rdtsc() - dec_start_time;
+        double dec_time = timer() - start_time;
+        printf("%lld clocks, %.1f clocks/symbol (%5.1fMB/s)\n", dec_clocks, 1.0 * dec_clocks / in_size, 1.0 * in_size / (dec_time * 1048576.0));
+    }
+
+    // check decode results
+    if (memcmp(in_bytes, dec_bytes, in_size) == 0)
+        printf("decode ok!\n");
+    else
+        printf("ERROR: bad decoder!\n");
+
+    // ---- SIMD interleaved rANS encode/decode.
+
+    memset(dec_bytes, 0xcc, in_size);
+
+    // try SIMD rANS encode
+    printf("\ninterleaved SIMD rANS encode: (encode itself isn't SIMD)\n");
+    for (int run=0; run < 5; run++) {
+        double start_time = timer();
+        uint64_t enc_start_time = __rdtsc();
+
+        RansWordEnc rans[8];
+        for (int i=0; i < 8; i++)
+            rans[i] = RansWordEncInit();
+
+        uint16_t* ptr = (uint16_t *)(out_buf + out_max_size); // *end* of output buffer
+
+        // last few bytes
+        for (size_t i=in_size; (i & 7) != 0; i--) { // NB: working in reverse
+            int s = in_bytes[i - 1];
+            RansWordEncPut(&rans[(i - 1) & 7], &ptr, stats.cum_freqs[s], stats.freqs[s]);
+        }
+
+        for (size_t i=(in_size & ~7); i > 0; i -= 2) { // NB: working in reverse!
+            int s1 = in_bytes[i-1];
+            int s0 = in_bytes[i-2];
+            int b = (i-2) & 7;
+            RansWordEncPut(&rans[b+1], &ptr, stats.cum_freqs[s1], stats.freqs[s1]);
+            RansWordEncPut(&rans[b+0], &ptr, stats.cum_freqs[s0], stats.freqs[s0]);
+        }
+
+        for (int i=8; i > 0; i--)
+            RansWordEncFlush(&rans[i - 1], &ptr);
+        rans_begin = ptr;
+
+        uint64_t enc_clocks = __rdtsc() - enc_start_time;
+        double enc_time = timer() - start_time;
+        printf("%lld clocks, %.1f clocks/symbol (%5.1fMiB/s)\n", enc_clocks, 1.0 * enc_clocks / in_size, 1.0 * in_size / (enc_time * 1048576.0));
+    }
+    printf("SIMD rANS: %d bytes\n", (int) (out_buf + out_max_size - (uint8_t*)rans_begin));
+
+    // try SIMD rANS decode
+    for (int run=0; run < 5; run++) {
+        double start_time = timer();
+        uint64_t dec_start_time = __rdtsc();
+
+        RansSimdDec rans0, rans1;
+        uint16_t* ptr = rans_begin;
+        RansSimdDecInit(&rans0, &ptr);
+        RansSimdDecInit(&rans1, &ptr);
+
+        for (size_t i=0; i < (in_size & ~7); i += 8) {
+            uint32_t s03 = RansSimdDecSym(&rans0, &tab);
+            uint32_t s47 = RansSimdDecSym(&rans1, &tab);
+            *(uint32_t *)(dec_bytes + i) = s03;
+            *(uint32_t *)(dec_bytes + i + 4) = s47;
+            RansSimdDecRenorm(&rans0, &ptr);
+            RansSimdDecRenorm(&rans1, &ptr);
+        }
+
+        // last few bytes
+        for (size_t i=(in_size & ~7); i < in_size; i++) {
+            RansSimdDec* which = (i & 4) != 0 ? &rans1 : &rans0;
+            uint8_t s = RansWordDecSym(&which->m128i_u32[i & 3], &tab);
+            dec_bytes[i] = s;
         }
 
         uint64_t dec_clocks = __rdtsc() - dec_start_time;
