bf027b56898578434a1ca82be3fb3b1406b2e06c
Michael Lettrich <michael.lettrich@cern.ch>
2019-02-13 16:45:34 +0100

    interleaved RANS64

--- main_nonchar.cpp
+++ main_nonchar.cpp
@@ -14,7 +14,7 @@
 //#include "SymbolStats.h"
 // This is just the sample program. All the meat is in rans_byte.h.
 
-using source_t = uint8_t;
+using source_t = int32_t;
 
 int main(int argc, char* argv[])
 {
@@ -125,82 +125,91 @@ int main(int argc, char* argv[])
     else
         printf("ERROR: bad decoder!\n");
 
-//    // ---- interleaved rANS encode/decode. This is the kind of thing you might do to optimize critical paths.
-//
-//    memset(dec_bytes.data(), 0xcc,tokens.size()*sizeof(source_t));
-//
-//    // try interleaved rANS encode
-//    printf("\ninterleaved rANS encode:\n");
-//    for (int run=0; run < 5; run++) {
-//        double start_time = timer();
-//        uint64_t enc_start_time = __rdtsc();
-//
-//        Rans64State rans0, rans1;
-//        Rans64EncInit(&rans0);
-//        Rans64EncInit(&rans1);
-//
-//        uint32_t* ptr = out_end;
-//
-//        // odd number of bytes?
-//        if (in_size & 1) {
-//            int s = in_bytes[in_size - 1];
-//            Rans64EncPutSymbol(&rans0, &ptr, &esyms[s], prob_bits);
-//        }
-//
-//        for (size_t i=(in_size & ~1); i > 0; i -= 2) { // NB: working in reverse!
-//            int s1 = in_bytes[i-1];
-//            int s0 = in_bytes[i-2];
-//            Rans64EncPutSymbol(&rans1, &ptr, &esyms[s1], prob_bits);
-//            Rans64EncPutSymbol(&rans0, &ptr, &esyms[s0], prob_bits);
-//        }
-//        Rans64EncFlush(&rans1, &ptr);
-//        Rans64EncFlush(&rans0, &ptr);
-//        rans_begin = ptr;
-//
-//        uint64_t enc_clocks = __rdtsc() - enc_start_time;
-//        double enc_time = timer() - start_time;
-//        printf("%" PRIu64" clocks, %.1f clocks/symbol (%5.1fMiB/s)\n", enc_clocks, 1.0 * enc_clocks / in_size, 1.0 * in_size / (enc_time * 1048576.0));
-//    }
-//    printf("interleaved rANS: %d bytes\n", (int) ((out_end - rans_begin) * sizeof(uint32_t)));
-//
-//    // try interleaved rANS decode
-//    for (int run=0; run < 5; run++) {
-//        double start_time = timer();
-//        uint64_t dec_start_time = __rdtsc();
-//
-//        Rans64State rans0, rans1;
-//        uint32_t* ptr = rans_begin;
-//        Rans64DecInit(&rans0, &ptr);
-//        Rans64DecInit(&rans1, &ptr);
-//
-//        for (size_t i=0; i < (in_size & ~1); i += 2) {
-//            uint32_t s0 = cum2sym[Rans64DecGet(&rans0, prob_bits)];
-//            uint32_t s1 = cum2sym[Rans64DecGet(&rans1, prob_bits)];
-//            dec_bytes[i+0] = (uint8_t) s0;
-//            dec_bytes[i+1] = (uint8_t) s1;
-//            Rans64DecAdvanceSymbolStep(&rans0, &dsyms[s0], prob_bits);
-//            Rans64DecAdvanceSymbolStep(&rans1, &dsyms[s1], prob_bits);
-//            Rans64DecRenorm(&rans0, &ptr);
-//            Rans64DecRenorm(&rans1, &ptr);
-//        }
-//
-//        // last byte, if number of bytes was odd
-//        if (in_size & 1) {
-//            uint32_t s0 = cum2sym[Rans64DecGet(&rans0, prob_bits)];
-//            dec_bytes[in_size - 1] = (uint8_t) s0;
-//            Rans64DecAdvanceSymbol(&rans0, &ptr, &dsyms[s0], prob_bits);
-//        }
-//
-//        uint64_t dec_clocks = __rdtsc() - dec_start_time;
-//        double dec_time = timer() - start_time;
-//        printf("%" PRIu64" clocks, %.1f clocks/symbol (%5.1fMB/s)\n", dec_clocks, 1.0 * dec_clocks / in_size, 1.0 * in_size / (dec_time * 1048576.0));
-//    }
-//
-//    // check decode results
-//    if (memcmp(in_bytes, dec_bytes, in_size) == 0)
-//        printf("decode ok!\n");
-//    else
-//        printf("ERROR: bad decoder!\n");
+    // ---- interleaved rANS encode/decode. This is the kind of thing you might do to optimize critical paths.
+
+    memset(dec_bytes.data(), 0xcc,tokens.size()*sizeof(source_t));
+
+    // try interleaved rANS encode
+    printf("\ninterleaved rANS encode:\n");
+    for (int run=0; run < 5; run++) {
+        double start_time = timer();
+        uint64_t enc_start_time = __rdtsc();
+
+        Rans64State rans0, rans1;
+        Rans64EncInit(&rans0);
+        Rans64EncInit(&rans1);
+
+        uint32_t* ptr = out_end;
+
+        // odd number of bytes?
+        if (tokens.size() & 1) {
+            const int s = tokens.back();
+            const size_t normalized = s - stats.min;
+            Rans64EncPutSymbol(&rans0, &ptr, &esyms[normalized], prob_bits);
+        }
+
+        for (size_t i=(tokens.size() & ~1); i > 0; i -= 2) { // NB: working in reverse!
+            const int s1 = tokens[i-1];
+            const int s0 = tokens[i-2];
+            const size_t normalized1 = s1 - stats.min;
+            const size_t normalized0 = s0 - stats.min;
+
+            Rans64EncPutSymbol(&rans1, &ptr, &esyms[normalized1], prob_bits);
+            Rans64EncPutSymbol(&rans0, &ptr, &esyms[normalized0], prob_bits);
+        }
+        Rans64EncFlush(&rans1, &ptr);
+        Rans64EncFlush(&rans0, &ptr);
+        rans_begin = ptr;
+
+        uint64_t enc_clocks = __rdtsc() - enc_start_time;
+        double enc_time = timer() - start_time;
+        printf("%" PRIu64" clocks, %.1f clocks/symbol (%5.1f MiB/s)\n", enc_clocks, 1.0 * enc_clocks / tokens.size()*enc_range, 1.0 * (tokens.size()*enc_range)  / (enc_time * 1048576.0));
+
+    }
+    printf("interleaved rANS: %d bytes\n", (int) ((out_end - rans_begin) * sizeof(uint32_t)));
+
+    // try interleaved rANS decode
+    for (int run=0; run < 5; run++) {
+        double start_time = timer();
+        uint64_t dec_start_time = __rdtsc();
+
+        Rans64State rans0, rans1;
+        uint32_t* ptr = rans_begin;
+        Rans64DecInit(&rans0, &ptr);
+        Rans64DecInit(&rans1, &ptr);
+
+        for (size_t i=0; i < (tokens.size() & ~1); i += 2) {
+            const uint32_t s0 = cum2sym[Rans64DecGet(&rans0, prob_bits)];
+            const uint32_t s1 = cum2sym[Rans64DecGet(&rans1, prob_bits)];
+            dec_bytes[i+0] = s0;
+            dec_bytes[i+1] = s1;
+            const size_t normalized0 = s0 - stats.min;
+            const size_t normalized1 = s1 - stats.min;
+            Rans64DecAdvanceSymbolStep(&rans0, &dsyms[normalized0], prob_bits);
+            Rans64DecAdvanceSymbolStep(&rans1, &dsyms[normalized1], prob_bits);
+            Rans64DecRenorm(&rans0, &ptr);
+            Rans64DecRenorm(&rans1, &ptr);
+        }
+
+        // last byte, if number of bytes was odd
+        if (tokens.size() & 1) {
+        	const uint32_t s0 = cum2sym[Rans64DecGet(&rans0, prob_bits)];
+            dec_bytes[tokens.size() - 1] = s0;
+            const size_t normalized = s0 - stats.min;
+            Rans64DecAdvanceSymbol(&rans0, &ptr, &dsyms[normalized], prob_bits);
+        }
+
+        uint64_t dec_clocks = __rdtsc() - dec_start_time;
+        double dec_time = timer() - start_time;
+        printf("%" PRIu64" clocks, %.1f clocks/symbol (%5.1f MiB/s)\n", dec_clocks, 1.0 * dec_clocks / tokens.size()*enc_range, 1.0 * tokens.size()*enc_range / (dec_time * 1048576.0));
+
+    }
+
+    // check decode results
+    if (memcmp(tokens.data(), dec_bytes.data(), tokens.size()*sizeof(source_t)) == 0)
+        printf("decode ok!\n");
+    else
+        printf("ERROR: bad decoder!\n");
 
     delete[] out_buf;
     return 0;
